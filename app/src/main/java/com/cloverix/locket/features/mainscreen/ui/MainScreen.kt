5382e1199aebee93ac4b5b57a946fed21b99eca2
Cloverix <efremenko160107@mail.ru>
2025-12-25 22:30:44 +0300

    Initial commit

---
+++ app/src/main/java/com/cloverix/locket/features/mainscreen/ui/MainScreen.kt
@@ -0,0 +1,86 @@
+package com.cloverix.locket.features.mainscreen.ui
+
+import android.content.Context
+import android.graphics.Paint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.platform.LocalWindowInfo
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.cloverix.locket.R
+
+@Composable
+fun MainScreen() {
+    //Background
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorResource(R.color.black))
+    ) {
+        Column {
+            //Top row: profile, friends and chat buttons
+            Row (
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .systemBarsPadding()
+            ) {
+                //Profile button
+                Button(
+                    onClick = {}
+                ) {}
+                //Friends button
+                Button(
+                    onClick = {},
+                    modifier = Modifier
+                        .wrapContentSize()
+                ) {
+                    Text(
+                        text = "Friends",
+                        fontSize = 20.sp
+                    )
+                }
+                //Chat button
+                Button(
+                    onClick = {}
+                ) {}
+            }
+            Spacer(Modifier.height(20.dp))
+            BoxWithConstraints {
+                Box(
+                    Modifier
+                        .fillMaxWidth()
+                        .height(maxWidth)
+                        .background(colorResource(R.color.white))
+                )
+            }
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun PreviewMainScreen() {
+    MainScreen()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,35 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    plugins {
+        kotlin("jvm") version "2.2.0"
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+dependencyResolutionManagement {
+    repositories {
+        // Add your repository here instead
+        maven {
+            name = "MavenRepo"
+            url = uri("https://your-repo-url.com") // Add your actual URL
+        }
+        // Other repositories like mavenCentral, google, etc.
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Locket"
+include(":app")
+ 
