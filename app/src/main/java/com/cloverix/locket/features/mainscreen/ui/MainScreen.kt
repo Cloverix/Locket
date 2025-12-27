bb3a9e4876b0e4328060c13ba1c2f2ec9612cd0c
Cloverix <efremenko160107@mail.ru>
2025-12-27 23:36:30 +0300

    added ui for a button that takes a photo; other unsignificant tweaks

--- app/src/main/java/com/cloverix/locket/features/mainscreen/ui/MainScreen.kt
+++ app/src/main/java/com/cloverix/locket/features/mainscreen/ui/MainScreen.kt
@@ -1,5 +1,7 @@
 package com.cloverix.locket.features.mainscreen.ui
 
+import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,9 +12,12 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Icon
@@ -20,11 +25,17 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.colorResource
@@ -39,6 +50,7 @@ import com.cloverix.locket.ui.theme.displayFontFamily
 
 @Composable
 fun MainScreen() {
+    var isFlashEnabled by remember { mutableStateOf(false) }
     //Background
     Surface (
         color = MaterialTheme.colorScheme.background,
@@ -54,14 +66,19 @@ fun MainScreen() {
                 modifier = Modifier
                     .fillMaxWidth()
                     .systemBarsPadding()
+                    .padding(vertical = 5.dp)
             ) {
                 //Profile button
                 Button(
-                    onClick = {}
+                    onClick = {
+                        //navController takes to profile screen
+                    }
                 ) {}
                 //Friends button
                 Button(
-                    onClick = {},
+                    onClick = {
+                        //navController takes to friends screen
+                    },
                     modifier = Modifier
                         .wrapContentSize()
                 ) {
@@ -72,7 +89,9 @@ fun MainScreen() {
                 }
                 //Chat button
                 Button(
-                    onClick = {}
+                    onClick = {
+                        //navCompose takes to chat screen
+                    }
                 ) {}
             }
             Spacer(Modifier.height(20.dp))
@@ -95,21 +114,48 @@ fun MainScreen() {
                     .fillMaxWidth()
                     .wrapContentHeight()
             ) {
-                val isFlashEnabled = rememberSaveable{ mutableStateOf(false) }
                 //Flash button
                 Icon(
-                    imageVector = ImageVector.vectorResource(if (isFlashEnabled.value) R.drawable.lightning_icon_filled else R.drawable.lightning_icon_outlined),
+                    imageVector = ImageVector.vectorResource(if (isFlashEnabled) R.drawable.lightning_icon_filled else R.drawable.lightning_icon_outlined),
                     contentDescription = "Lightning icon",
-                    tint = colorResource(if (isFlashEnabled.value) R.color.enabled_flash_icon_color else R.color.white),
+                    tint = colorResource(if (isFlashEnabled) R.color.yellow else R.color.white),
                     modifier = Modifier
                         .scale(1.5f)
                         .clickable(
                             onClick = {
-                                //Function that enables / disables the flash and changes the isFlashEnabled parameter
+                                //Function that enables / disables the flash and changes isFlashEnabled parameter
+                                isFlashEnabled = !isFlashEnabled
                             }
                         )
                 )
-                //TODO: Add a button for taking a photo
+                //Take a photo button
+                Box(
+                    modifier = Modifier
+                        .size(105.dp)
+                        .clip(CircleShape)
+                        .background(MaterialTheme.colorScheme.primary)
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .size(95.dp)
+                            .clip(CircleShape)
+                            .background(colorResource(R.color.black))
+                            .align(Alignment.Center)
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .size(80.dp)
+                                .clip(CircleShape)
+                                .background(colorResource(R.color.yellow))
+                                .align(Alignment.Center)
+                                .clickable(
+                                    onClick = {
+                                        //Function that takes a photo
+                                    }
+                                )
+                        )
+                    }
+                }
                 //Camera flip button
                 Icon(
                     imageVector = ImageVector.vectorResource(R.drawable.arrows_clockwise_fill),
