8e840f8cd722590fde076be3988f897d1d680611
Cloverix <efremenko160107@mail.ru>
2025-12-30 13:44:31 +0300

    minor visual changes

--- app/src/main/java/com/cloverix/locket/features/mainscreen/presentation/screen/MainScreen.kt
+++ app/src/main/java/com/cloverix/locket/features/mainscreen/presentation/screen/MainScreen.kt
@@ -1,32 +1,24 @@
 package com.cloverix.locket.features.mainscreen.presentation.screen
 
-import android.content.res.Resources
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonElevation
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -37,15 +29,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.cloverix.locket.R
@@ -67,7 +57,7 @@ fun MainScreen(navController: NavHostController) {
                     .fillMaxWidth()
                     .wrapContentHeight()
                     .systemBarsPadding()
-                    .padding(vertical = 5.dp)
+                    .padding(vertical = 10.dp)
             ) {
                 //Profile button
                 Box(
@@ -81,7 +71,14 @@ fun MainScreen(navController: NavHostController) {
                             }
                         )
                 ) {
-                    //Circular profile image here
+                    Icon(
+                        imageVector = ImageVector.vectorResource(R.drawable.user_circle),
+                        contentDescription = "Chat icon",
+                        tint = colorResource(R.color.black),
+                        modifier = Modifier
+                            .align(Alignment.Center)
+                            .scale(1.25f)
+                    )
                 }
                 //Friends button
                 Button(
@@ -93,7 +90,8 @@ fun MainScreen(navController: NavHostController) {
                 ) {
                     Text(
                         text = "Friends",
-                        fontSize = 20.sp
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold
                     )
                 }
                 //Chat button
@@ -117,7 +115,7 @@ fun MainScreen(navController: NavHostController) {
                     )
                 }
             }
-            Spacer(Modifier.height(20.dp))
+            Spacer(Modifier.height(15.dp))
             //Place for camera image
             BoxWithConstraints {
                 Box(
@@ -139,7 +137,7 @@ fun MainScreen(navController: NavHostController) {
             ) {
                 //Flash button
                 Icon(
-                    imageVector = ImageVector.vectorResource(if (isFlashEnabled) R.drawable.lightning_icon_filled else R.drawable.lightning_icon_outlined),
+                    imageVector = ImageVector.vectorResource(if (isFlashEnabled) R.drawable.lightning_filled else R.drawable.lightning_outlined),
                     contentDescription = "Lightning icon",
                     tint = colorResource(if (isFlashEnabled) R.color.yellow else R.color.white),
                     modifier = Modifier
@@ -197,7 +195,7 @@ fun MainScreen(navController: NavHostController) {
             Spacer(Modifier.height(60.dp))
             Button(
                 onClick = {
-
+                    //Opens photos history
                 },
                 modifier = Modifier
                     .wrapContentSize()
@@ -209,7 +207,7 @@ fun MainScreen(navController: NavHostController) {
                     Text(
                         text = "History",
                         fontSize = 20.sp,
-                        modifier = Modifier
+                        fontWeight = FontWeight.SemiBold
                     )
                     Icon(
                         imageVector = ImageVector.vectorResource(R.drawable.caret_down),
